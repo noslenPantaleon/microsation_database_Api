5be05e318a4791401881bdf7b2db644149303be9
alexrobaina <alexrobainaph@gmail.com>
2020-11-02 23:42:24 -0300

    apanay start

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import routerx from 'express-promise-router';
+import sensorRouter from './sensor.routes';
 import authRouter from './auth.routes';
-import petRouter from './pet.routes';
-import privateRouter from './private.routes';
 
 const router = routerx();
 
-router.use('', authRouter);
+router.use('/api', sensorRouter);
+router.use('/api', authRouter);
 
 export default router;
